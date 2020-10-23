9d8891b0b09ffbe459e1406efaf2f324f55cbf1f
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-23 20:29:41 +0200

    Add img, title, excerpt, overlay into post slug

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import { getSinglePost } from '../../store/actions/postActions'
+import styles from './[slug].module.css'
 
 const PostPage = () => {
   const dispatch = useDispatch()
@@ -16,16 +17,28 @@ const PostPage = () => {
     dispatch(getSinglePost(slug))
   }, [dispatch])
   return (
-    <article>
+    <article className={styles.postPage}>
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <div>
-          <h1>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </div>
+        <>
+          <div
+            className={styles.postImg}
+            style={{
+              backgroundImage: `url(${post.feature_image})`,
+            }}
+          >
+            <h1 className={styles.postTitle}>{post.title}</h1>
+            <div className={styles.postImgOverlay} />
+          </div>
+          <p className={styles.postExcerpt}>{post.excerpt}</p>
+          <div
+            className={styles.postHtml}
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </>
       )}
     </article>
   )
