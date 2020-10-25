0ccbdb411979d18bf35d059f20eb9024c2ea542c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-25 17:30:22 +0100

    Rewrite PostPage

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -2,7 +2,51 @@ import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import { getSinglePost } from '../../store/actions/postActions'
-import styles from './[slug].module.css'
+
+import styled from 'styled-components'
+
+const Article = styled.article`
+  margin: 30px;
+`
+
+const ArticleImg = styled.div`
+  position: relative;
+  width: 100%;
+  height: 300px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`
+
+const ArticleTitle = styled.h1`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  color: var(--light);
+  margin: 0;
+`
+
+const ImgOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.466);
+  z-index: 99;
+`
+
+const ArticleExcerpt = styled.p`
+  font-size: 1.2rem;
+`
+
+const ArticleContent = styled.div``
 
 const PostPage = () => {
   const dispatch = useDispatch()
@@ -17,30 +61,26 @@ const PostPage = () => {
     dispatch(getSinglePost(slug))
   }, [dispatch, slug])
   return (
-    <article className={styles.postPage}>
+    <Article>
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
         <div>{error}</div>
       ) : (
         <>
-          <div
-            className={styles.postImg}
+          <ArticleImg
             style={{
               backgroundImage: `url(${post.feature_image})`,
             }}
           >
-            <h1 className={styles.postTitle}>{post.title}</h1>
-            <div className={styles.postImgOverlay} />
-          </div>
-          <p className={styles.postExcerpt}>{post.excerpt}</p>
-          <div
-            className={styles.postHtml}
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
+            <ArticleTitle>{post.title}</ArticleTitle>
+            <ImgOverlay />
+          </ArticleImg>
+          <ArticleExcerpt>{post.excerpt}</ArticleExcerpt>
+          <ArticleContent dangerouslySetInnerHTML={{ __html: post.html }} />
         </>
       )}
-    </article>
+    </Article>
   )
 }
 
