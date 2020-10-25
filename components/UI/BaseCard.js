d0ff5d08c53e028f7b09e9e6d414defd5159823c
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-25 23:14:13 +0100

    Modiufy BaseCard, add border left

--- components/UI/BaseCard.js
+++ components/UI/BaseCard.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components'
+
 export const Card = styled.div`
   max-width: 100%;
-  height: 300px;
+  min-height: 300px;
   margin: 30px;
-  background-color: var(--orange);
+  background-color: transparent;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  box-shadow: 0 0 1rem rgba(0, 0, 0, 0.4);
   transition: transform 0.3s ease-in-out;
   &:hover {
     transform: translateY(-10px);
@@ -19,17 +19,28 @@ export const ImgTop = styled.img`
   object-fit: cover;
   height: 250px;
   width: 100%;
+  border-radius: 10px;
 `
 
 export const CardBody = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   width: 100%;
-  height: 50px;
+  height: 100px;
 `
 
 export const CardTitle = styled.h4`
   text-transform: uppercase;
+  color: var(--light);
+  font-size: 1.5rem;
+  transition: border 0.3s ease-out;
+  &::before {
+    content: '';
+    border-left: 3px solid var(--orange);
+    border-width: 10px;
+    margin-right: 10px;
+    overflow: hidden;
+  }
 `
