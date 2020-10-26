800db5baea3e8e4e65c6b0a7562dd8f411a74775
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-26 18:48:41 +0100

    Add Artivcle component

---
+++ components/article/Article.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ArticleImg = styled.div`
+  position: relative;
+  width: 100%;
+  height: 300px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  border-radius: 10px;
+`
+
+const ArticleTitle = styled.h1`
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+  color: var(--light);
+  text-transform: uppercase;
+
+  margin: 0;
+`
+
+const ImgOverlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.466);
+  z-index: 99;
+`
+
+const ArticleExcerpt = styled.p`
+  font-size: 1.2rem;
+  color: var(--light);
+`
+
+const ArticleContent = styled.div`
+  color: var(--light);
+
+  & > p {
+    font-size: 1.3rem;
+    & > strong {
+      color: var(--orange);
+    }
+  }
+`
+
+const Article = ({ data }) => {
+  return (
+    <div style={{ margin: '30px' }}>
+      <ArticleImg
+        style={{
+          backgroundImage: `url(${data.feature_image})`
+        }}
+      >
+        <ArticleTitle>{data.title}</ArticleTitle>
+        <ImgOverlay />
+      </ArticleImg>
+      <ArticleExcerpt>{data.excerpt}</ArticleExcerpt>
+      <ArticleContent dangerouslySetInnerHTML={{ __html: data.html }} />
+    </div>
+  )
+}
+
+export default Article
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,64 +3,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { useRouter } from 'next/router'
 import { getSinglePost } from '../../store/actions/postActions'
 
-import styled from 'styled-components'
 import BaseLoader from '../../components/UI/BaseLoader'
-
-const Article = styled.article`
-  margin: 30px;
-`
-
-const ArticleImg = styled.div`
-  position: relative;
-  width: 100%;
-  height: 300px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  border-radius: 10px;
-`
-
-const ArticleTitle = styled.h1`
-  position: absolute;
-  width: 100%;
-  text-align: center;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  z-index: 100;
-  color: var(--light);
-  text-transform: uppercase;
-
-  margin: 0;
-`
-
-const ImgOverlay = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.466);
-  z-index: 99;
-`
-
-const ArticleExcerpt = styled.p`
-  font-size: 1.2rem;
-  color: var(--light);
-`
-
-const ArticleContent = styled.div`
-  color: var(--light);
-
-  & > p {
-    font-size: 1.3rem;
-    & > strong {
-      color: var(--orange);
-    }
-  }
-`
+import Article from '../../components/article/Article'
 
 const PostPage = () => {
   const dispatch = useDispatch()
@@ -75,26 +19,15 @@ const PostPage = () => {
     dispatch(getSinglePost(slug))
   }, [dispatch, slug])
   return (
-    <Article>
+    <>
       {loading ? (
         <BaseLoader />
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <>
-          <ArticleImg
-            style={{
-              backgroundImage: `url(${post.feature_image})`
-            }}
-          >
-            <ArticleTitle>{post.title}</ArticleTitle>
-            <ImgOverlay />
-          </ArticleImg>
-          <ArticleExcerpt>{post.excerpt}</ArticleExcerpt>
-          <ArticleContent dangerouslySetInnerHTML={{ __html: post.html }} />
-        </>
+        <Article data={post} />
       )}
-    </Article>
+    </>
   )
 }
 
