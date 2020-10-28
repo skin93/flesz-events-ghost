01fac2be111d1004539413a12325c626be59202d
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-28 11:28:02 +0100

    Move FeaturedPostsContainer into FeaturedPosts

--- components/posts/FeaturedPosts.js
+++ components/posts/FeaturedPosts.js
@@ -1,19 +1,33 @@
 import React from 'react'
-import Link from 'next/link'
 import FeaturedPostItem from './FeaturedPostItem'
-import FeaturedPostsContainer from './FeaturedPostsContainer'
+
+import styled from 'styled-components'
+import { device } from '../../constants/device'
+
+const FeaturedPostsContainer = styled.div`
+  display: grid;
+  gap: 10px;
+
+  @media ${device.mobileS} {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  @media ${device.tablet} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media ${device.laptopL} {
+    grid-template-columns: repeat(1, 1fr);
+    /* justify-items: center;
+    align-items: center; */
+  }
+`
 
 const FeaturedPosts = ({ featured }) => {
   return (
     <FeaturedPostsContainer>
       {featured?.map((feat) => (
-        <div key={feat.id}>
-          <Link href={`/posts/${feat.slug}`}>
-            <a>
-              <FeaturedPostItem featured={feat} />
-            </a>
-          </Link>
-        </div>
+        <FeaturedPostItem feat={feat} />
       ))}
     </FeaturedPostsContainer>
   )
