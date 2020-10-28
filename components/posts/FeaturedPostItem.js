3e1ee368ae07f16e99972ea6c7824a82cb5bfbaa
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-10-28 11:28:49 +0100

    Add FeaturedCard

--- components/posts/FeaturedPostItem.js
+++ components/posts/FeaturedPostItem.js
@@ -1,14 +1,48 @@
+import Link from 'next/link'
 import React from 'react'
-import { Card, CardBody, CardTitle, ImgTop } from '../UI/BaseCard'
 
-const FeaturedPostItem = ({ featured }) => {
+import styled from 'styled-components'
+
+const FeaturedCard = styled.div`
+  width: 100%;
+  min-height: 100px;
+  padding: 10px;
+  background-color: transparent;
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+  transition: transform 0.3s ease-in-out;
+  &:hover {
+    transform: translateY(-10px);
+  }
+`
+
+export const FeaturedImg = styled.img`
+  object-fit: cover;
+  height: 100%;
+  width: 100px;
+  border-radius: 5px;
+`
+
+export const FeatTitle = styled.h4`
+  text-transform: uppercase;
+  margin: 0;
+  margin-left: 20px;
+  color: var(--light);
+  font-size: 1rem;
+  transition: border 0.3s ease-out;
+`
+
+const FeaturedPostItem = ({ feat }) => {
   return (
-    <Card>
-      <ImgTop src={featured.feature_image} alt='Feature image' />
-      <CardBody>
-        <CardTitle>{featured.title}</CardTitle>
-      </CardBody>
-    </Card>
+    <Link key={feat.id} href={`/posts/${feat.slug}`}>
+      <a>
+        <FeaturedCard>
+          <FeaturedImg src={feat.feature_image} alt='Feature image' />
+          <FeatTitle>{feat.title}</FeatTitle>
+        </FeaturedCard>
+      </a>
+    </Link>
   )
 }
 
