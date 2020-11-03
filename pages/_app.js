c8f6b3a64635271113f86f4517bd91ef4ecb47ce
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-11-03 22:08:06 +0100

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import { SWRConfig } from 'swr'
 import { GlobalStyles } from '../global'
 import { ThemeProvider } from 'styled-components'
+import Router from 'next/router'
 
 import { Layout } from '../components/index'
 import { theme } from '../theme'
+
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 const fetcher = async (url) => {
   const res = await fetch(url)
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
@@ -14,8 +16,12 @@ export default class MyDocument extends Document {
         })
 
       const initialProps = await Document.getInitialProps(ctx)
+
+      const isProduction = process.env.NODE_ENV === 'production'
+
       return {
         ...initialProps,
+        isProduction,
         styles: (
           <>
             {initialProps.styles}
@@ -28,9 +34,31 @@ export default class MyDocument extends Document {
     }
   }
   render() {
+    const { isProduction } = this.props
     return (
       <Html lang='pl'>
         <Head />
+        {isProduction && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `
+              }}
+            />
+          </>
+        )}
         <body>
           <Main />
           <NextScript />
