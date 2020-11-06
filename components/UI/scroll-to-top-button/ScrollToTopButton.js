09874e44a3d7d4640c2b4315099c254206c41eb3
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-11-06 10:00:38 +0100

    Add animation to ScrollToTopButton

--- components/UI/scroll-to-top-button/ScrollToTopButton.js
+++ components/UI/scroll-to-top-button/ScrollToTopButton.js
@@ -1,4 +1,18 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const upAndDown = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(-50px);
+  }
+
+  100% {
+    transform: translateY(0)
+  }
+`
 
 export default styled.div`
   opacity: ${(props) => (props.showButton ? 1 : 0)};
@@ -15,4 +29,5 @@ export default styled.div`
   border-radius: 10px;
   font-size: 18px;
   transition: opacity 0.3s ease-in-out;
+  animation: ${upAndDown} 3s infinite;
 `
